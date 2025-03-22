585dd205bf55a12d92e7bfef4a594a7844b7f2ae
vikasgodara290 <vikasgodara290@gmail.com>
2025-03-22 09:10:03 +0530

    added words/word/letter components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import english10k from './assets/english_10k.json'
 
 function App() {
@@ -23,17 +24,47 @@ function Words(props : WordsType){
   }
 
   return(
-    <div className='flex items-center h-screen'>
-    <div className='flex w-3/4 flex-wrap text-4xl mx-auto '>
+    <div className='flex items-center h-screen' contentEditable>
+    <div className='flex w-3/4 flex-wrap text-4xl mx-auto text-gray-500'>
       {
         Words.map(
           (word, index) => {
-            return <div key={index} className='mr-6 mb-7 h-7 flex items-center'>{word}</div>
+            return <Word word={word} index={index}/>
           }
         )
       }
     </div>  
     </div>
+  )
+}
+
+interface WordType {
+  word : string,
+  index : number
+}
+function Word({word, index} : WordType){
+  
+  return (
+    <div key={index} className='mr-6 mb-7 h-7 flex items-center'>
+      {
+        word.split('').map(
+          (letter, index) => (
+            <Letter letter={letter} index={index}/>
+          )
+        )
+      }
+    </div>
+  )
+}
+
+interface LetterType {
+  letter : string,
+  index : number
+}
+function Letter({letter, index} : LetterType){
+  const letterRef = useRef(null);
+  return(
+    <p key={index} ref={letterRef}>{letter}</p>
   )
 }
 
